4f9e5147f7b82fb84f84a007bb8bbc8539226943
Sukijaa <sukijaavs@gmail.com>
2025-05-08 23:44:47 +0530

    show feedback

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -4,9 +4,11 @@ import dayjs from 'dayjs';
  import {Button} from "@/components/ui/button";
  import Link from "next/link";
  import DisplayTechIcons from "@/components/DisplayTechIcons";
- 
- const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
-     const feedback = null as Feedback | null;
+ import {getFeedbackByInterviewId} from "@/lib/actions/general.action";
+ const InterviewCard = async ({ id, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
+    const feedback = userId && id ?
+        await getFeedbackByInterviewId({ interviewId: id, userId })
+        : null;
      const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
      const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY');
  
@@ -46,8 +48,8 @@ import dayjs from 'dayjs';
  
                      <Button className="btn-primary">
                          <Link href={feedback
-                             ? `/interview/${interviewId}/feedback`
-                             : `/interview/${interviewId}`
+                             ? `/interview/${id}/feedback`
+                             : `/interview/${id}`
                          }>
                              {feedback ? 'Check Feedback' : 'View Interview'}
                          </Link>
