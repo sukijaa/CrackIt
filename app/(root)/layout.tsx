3b69f83c0b86b9c88b3314eed7b495307eaa1c39
Sukijaa <sukijaavs@gmail.com>
2025-05-08 21:46:14 +0530

    generated interviews on home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -15,7 +15,7 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
              <nav>
                  <Link href="/"  className="flex items-center gap-2" >
                      <Image src="/logo.svg" alt="Logo" width={38} height={32} />
-                     <h2 className="text-primary-100">PrepWise</h2>
+                     <h2 className="text-primary-100">CrackIt</h2>
                  </Link>
              </nav>
  
